33e9eddea87daa6d05770a9e582c2fb532c00527
Filip Skukan <skukan159@gmail.com>
2019-09-24 14:22:26 +0200

    Added tests

---
+++ Part2/classes.test.js
@@ -0,0 +1,18 @@
+const { Event } = require('./Event'); //import Event from './Event'
+const { DataType } = require('./DataType')//import DataType from './DataType'
+//import WeatherData from './WeatherData'
+
+
+test('Test of my test runner', () => {
+    expect(1+1).toBe(2)
+})
+
+test('Event class tests', () => {
+    let date = new Date()
+    let event = new Event(date,"Horsens");
+
+    expect(event.time()).toBe(date);
+    expect(event.place()).toBe("Horsens");
+})
+
+
